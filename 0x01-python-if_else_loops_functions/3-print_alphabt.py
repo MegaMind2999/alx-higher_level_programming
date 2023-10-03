8ae62e100a471286de1b418af42179fed01af8fe
Abdelrhman Mohamed <abdo.mohamed29999@gmail.com>
2023-10-03 15:04:13 -0700

    new

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for c in range(97, 123):
-    l=chr(c)
-    if l != 'q' and l != 'e':
-        print("{}".format(l), end="")
+    lc = chr(c)
+    if lc != 'q' and lc != 'e':
+        print("{}".format(lc), end="")
