398e58538fbb69f00f91855006c585c4b345a8b5
Abdelrhman Mohamed <abdo.mohamed29999@gmail.com>
2023-10-05 14:27:49 -0700

    new

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    """Python function that does exactly the same as the Python bytecode"""
+    """Matching bytecode provided"""
     from magic_calculation_102 import add, sub
 
     if a < b:
@@ -11,4 +11,4 @@ def magic_calculation(a, b):
         return (c)
 
     else:
-        return(sub(a, b))
+        return (sub(a, b))
