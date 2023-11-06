39ca7642c3f9e7b7b595a70d961ad122ea2a2a17
Abdelrhman Mohamed <abdo.mohamed29999@gmail.com>
2023-11-06 14:07:05 -0800

    new

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
-"""Defines an object attribute lookup function"""
+#!/usr/bin/python3
+"""lookup module."""
 
 
 def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))
+    """lookup method
+    Returns: a list of available attributes and methods of an object"""
+    return dir(obj)
